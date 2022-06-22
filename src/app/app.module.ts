bb588007893b0055b476637d93ac9c0eb3c3a1e9
rohanjdhv550 <rohanjdhv97@gmail.com>
2022-06-22 13:10:46 +0530

    included popper/core package and implemented the pagination to the table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxEchartsModule } from 'ngx-echarts';
+import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +24,7 @@ import { TablesComponent } from './tables/tables.component';
     NgxEchartsModule.forRoot({
       echarts: ()=> import('echarts'),
     }),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tables/tables.component.ts
+++ src/app/tables/tables.component.ts
@@ -12,8 +12,8 @@ export class TablesComponent{
 // table variables
   showRow = true;
   rowPerPage = 5;
-  tmpRowCount = this.rowPerPage;
-
+  page = 1;
+  
 //table matrix [array] data
   tableDataArr = [
     ['Tiger Nixon','System Architect','Edinburgh','61','2011/04/25','$320,800'],
@@ -21,21 +21,21 @@ export class TablesComponent{
     ['Ashton Cox','Junior Technical Author','San Francisco','66','2009/01/12','$86,000'],
     ['Cedric Kelly','Senior Javascript Developer','Edinburgh','22','2012/03/29','$433,060'],
     ['Airi Satou','Accountant','Edinburgh','61','2011/04/25','$320,800'],
-    ['Tiger Nixon','System Architect','Edinburgh','61','2011/04/25','$320,800'],
-    ['Garrett Winters','Accountant','Tokyo','63','2011/07/25','$170,750'],
-    ['Ashton Cox','Junior Technical Author','San Francisco','66','2009/01/12','$86,000'],
+    ['Manoj','System Architect','Edinburgh','61','2011/04/25','$320,800'],
+    ['Kunal','Accountant','Tokyo','63','2011/07/25','$170,750'],
+    ['Shankar','Junior Technical Author','San Francisco','66','2009/01/12','$86,000'],
+    ['Vishu','Senior Javascript Developer','Edinburgh','22','2012/03/29','$433,060'],
+    ['Kamath','Accountant','Edinburgh','61','2011/04/25','$320,800'],
+    ['Manu','System Architect','Edinburgh','61','2011/04/25','$320,800'],
+    ['Manju','Accountant','Tokyo','63','2011/07/25','$170,750'],
+    ['Shiva','Junior Technical Author','San Francisco','66','2009/01/12','$86,000'],
+    ['Shesh','Senior Javascript Developer','Edinburgh','22','2012/03/29','$433,060'],
+    ['Sumanth','Accountant','Edinburgh','61','2011/04/25','$320,800'],
+    ['Sagar','System Architect','Edinburgh','61','2011/04/25','$320,800'],
+    ['Sandesh','Accountant','Tokyo','63','2011/07/25','$170,750'],
+    ['Chandler','Junior Technical Author','San Francisco','66','2009/01/12','$86,000'],
     ['Cedric Kelly','Senior Javascript Developer','Edinburgh','22','2012/03/29','$433,060'],
     ['Airi Satou','Accountant','Edinburgh','61','2011/04/25','$320,800'],
-    ['Tiger Nixon','System Architect','Edinburgh','61','2011/04/25','$320,800'],
-    ['Garrett Winters','Accountant','Tokyo','63','2011/07/25','$170,750'],
-    ['Ashton Cox','Junior Technical Author','San Francisco','66','2009/01/12','$86,000'],
-    ['Cedric Kelly','Senior Javascript Developer','Edinburgh','22','2012/03/29','$433,060'],
-    ['Airi Satou','Accountant','Edinburgh','61','2011/04/25','$320,800'],
-    ['Tiger Nixon','System Architect','Edinburgh','61','2011/04/25','$320,800'],
-    ['Garrett Winters','Accountant','Tokyo','63','2011/07/25','$170,750'],
-    ['Ashton Cox','Junior Technical Author','San Francisco','66','2009/01/12','$86,000'],
-    ['Cedric Kelly','Senior Javascript Developer','Edinburgh','22','2012/03/29','$433,060'],
-    ['Airi Satou','Accountant','Edinburgh','61','2011/04/25','$320,800']
   ];
 
 // functions
